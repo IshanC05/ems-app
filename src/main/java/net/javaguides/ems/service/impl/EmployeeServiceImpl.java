7f0d1534b16bebf63dafb2b6e16dd7dc1ccab2b5
Ishan <ishaanchugh05@gmail.com>
2024-10-07 20:56:52 +0530

    Created Get All Employees API

--- src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -9,6 +9,8 @@ import net.javaguides.ems.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 import java.lang.module.ResolutionException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -29,5 +31,13 @@ public class EmployeeServiceImpl implements EmployeeService {
                 .orElseThrow(
                         () -> new ResolutionException("Employee does not exist with given id : " + employeeId));
         return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream()
+                .map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
     }
 }
