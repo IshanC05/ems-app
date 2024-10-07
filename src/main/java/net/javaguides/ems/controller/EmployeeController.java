08ae635331c5f0aa3ea314f5326b4e88c631c2a7
Ishan <ishaanchugh05@gmail.com>
2024-10-07 22:12:48 +0530

    Added ExceptionHandler for one specific controller

--- src/main/java/net/javaguides/ems/controller/EmployeeController.java
+++ src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -2,11 +2,15 @@ package net.javaguides.ems.controller;
 
 import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.exception.ErrorDetails;
+import net.javaguides.ems.exception.ResourceNotFoundException;
 import net.javaguides.ems.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -50,5 +54,20 @@ public class EmployeeController {
     public ResponseEntity<String> deleteEmployeeById(@PathVariable("id") Long employeeId) {
         employeeService.deleteEmployeeById(employeeId);
         return ResponseEntity.ok("Employee deleted successfully!");
+    }
+
+    // handle specific exception for a controller
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "USER_NOT_FOUND"
+        );
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+
     }
 }
